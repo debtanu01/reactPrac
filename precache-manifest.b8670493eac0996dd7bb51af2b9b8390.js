83bc73fa8a76b4a84a783ab510acc44ffa79ae6b
debtanu01 <debtanu96roy@gmail.com>
2020-07-16 21:31:14 +0530

    react

---
+++ precache-manifest.b8670493eac0996dd7bb51af2b9b8390.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b198a9565d0435f507187debdccb4056",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd9c451c5263dcfb6048",
+    "url": "/static/css/main.22f1364a.chunk.css"
+  },
+  {
+    "revision": "5c5b3165c4d17a3153cc",
+    "url": "/static/js/2.7b0827cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7b0827cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd9c451c5263dcfb6048",
+    "url": "/static/js/main.d2ab619d.chunk.js"
+  },
+  {
+    "revision": "3b893e5fee77c0e29123",
+    "url": "/static/js/runtime-main.106d7e89.js"
+  }
+]);
